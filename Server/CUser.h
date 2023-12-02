9d88e82c4bcff8c3dbdc53084ab0d26a805cadc1
ShemberkoO <oleksandr.shemberko.pz.2022@lpnu.ua>
2023-12-02 22:27:54 +0200

    get user by id & message

--- Server/CUser.h
+++ Server/CUser.h
@@ -7,7 +7,9 @@ private:
 	char s_password[50] = "";
 	int i_picture = 0;
 public:
+
 	friend class CDatabase;
+	CUser();
 	CUser(char name[50], char password[50],int picture) {
 		std::memcpy(s_name, name, 50);
 		std::memcpy(s_password, password, 50);
